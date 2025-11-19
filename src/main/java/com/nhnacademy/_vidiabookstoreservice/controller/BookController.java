3e77fc004ac8356bfc906595afe3e935f65748a4
BaeJongOk <0511jongok@gmail.com>
2025-11-19 16:40:28 +0900

    hi

--- src/main/java/com/nhnacademy/_vidiabookstoreservice/controller/BookController.java
+++ src/main/java/com/nhnacademy/_vidiabookstoreservice/controller/BookController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class BookController {
     @GetMapping("/hello/books")
     public String getBook() {
+
         return "드디어 완성?";
     }
 }
